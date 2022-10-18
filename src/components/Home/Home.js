0a77c21781a6863367ae33fd4cbcc9ab82b661b5
Jackson Konkin <jacksonkonkin@gmail.com>
2022-10-18 10:32:19 -0700

    more home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,19 @@
+import { Outlet, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";  
+
 export default function Home() {
     return (
         <div>
             <header className="banner">
-            <h1>Welcome to Victoria</h1>
-        </header>
-        <main>
-            <h3>Your Journey starts Here</h3>
-            <button>Create a Profile</button>
-        </main>
+                <h1>Welcome to Victoria</h1>
+            </header>
+            <main>
+                <h3><span>Your</span> journey starts here</h3>
+                <div className="line"></div>
+                    <NavLink to="/Profile" activeClassName="current" exact>
+                        <li>Create a Profile</li>
+                    </NavLink>
+            </main>
         </div>
         
     );
