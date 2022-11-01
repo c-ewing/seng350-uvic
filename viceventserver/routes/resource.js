64046b03045c16eb6cdfe34d72496a111bc1b4d8
c-ewing <59128151+c-ewing@users.noreply.github.com>
2022-11-01 14:23:59 -0700

    Updated README to include draft response format. Update the resource router to use the iterator pattern and add request filtering

--- viceventserver/routes/resource.js
+++ viceventserver/routes/resource.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/:resourceType/:id?', function(req, res, next) {
-  res.json(req.params);
-});
+/*
+| Resource Type | Type name |
+| ------------- | --------- |
+| Sports Team   | sports-team |
+| Restaurant    | restaurant |
+| School Club   | school-club |
+| Event         | event     |
+| Victoria Resource | vic-resource |
+| Degree Specific Opportunity | degree-opportunity |
+| Job Opportunity | job     |
+*/
+const resource_types = ["sports-team", "restaurant", "school-club",
+  "event", "vic-resource", "degree-opportunity", "job"]
+
+const filter_req = function (req, res, next) {
+  // Check to make sure the resource type is valid
+  if (resource_types.some(resource => resource == req.params.resourceType)) {
+    // Handle the valid request
+    next()
+  } else {
+    // Bad request, 400 status and drop
+    res.status(400)
+    res.send("Bad Resource Type").end()
+  }
+}
+
+const handle_valid = function (req, res) {
+  res.send("Valid")
+}
+
+/* GET resource. */
+// First filter bad requests, then handle valid ones
+router.get('/:resourceType/:id?', [filter_req, handle_valid])
 
 module.exports = router;
