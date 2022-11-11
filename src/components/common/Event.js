e0128d1eb15eb741ecbcdad27653bba3cc41f393
berkanozturk <berkanozturk@ubuntu.myguest.virtualbox.org>
2022-11-10 22:55:46 -0800

    events

--- src/components/common/Event.js
+++ src/components/common/Event.js
@@ -17,14 +17,19 @@ function LongDescription({value, children, ...props }){
   const [showFirstElement, setShowFirstElement] = useState(false);
   const toggleFirstElement = () => setShowFirstElement(!showFirstElement);
 
+<div style={{border: "1px solid black", margin: "10px"}}></div>
+
+
   return (
     <>
+    <div >
       <Button onClick={toggleFirstElement}>Show More Details</Button>
       <MDBCollapse show={showFirstElement} className='mt-3'>
         <MDBCardText>
             {value}
         </MDBCardText>
       </MDBCollapse>
+      </div>
     </>
   )
 }
@@ -44,7 +49,7 @@ class Event extends Component {
             <MDBCardTitle>
                 {this.props.title}
             </MDBCardTitle>
-            <SaveDelete value={this.props.id} />
+           
             <MDBCardText>
                 {this.props.startDate}
             </MDBCardText>
@@ -58,6 +63,9 @@ class Event extends Component {
             <MDBCardText>
                 {this.props.image}
             </MDBCardText>
+            <div > 
+            <SaveDelete value={this.props.id} />
+            </div>
         </MDBCardBody>
     </MDBCard>
     )
