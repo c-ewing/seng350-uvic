6a5ed7cf3877071bdb87bb5cd123b5befa3d06c7
Rebecca Marshall <rebeccamarshall@uvic.ca>
2022-12-06 12:02:25 -0800

    add search results page and fix error in EventFetcher

--- src/components/Explore/SearchResults.js
+++ src/components/Explore/SearchResults.js
@@ -1,30 +1,50 @@
-import { useEffect, useState } from "react";
+import EventFetcher from "../common/EventFetcher";
+import Event from "../common/Event";
 
 export default function SearchResults () {
-    const [data, setData] = useState([]);
     var url = window.location.href
     const searchTerm = url.split('?').pop().replace('%20', ",");
     console.log(searchTerm)
 
-    useEffect(() => {
-            // fetch data
-            const dataFetch = async () => {
-            const data = await (
-                await fetch(
-                    `http://localhost:3000/search/${searchTerm}`
-                    )
-                ).json();
-
-                // set state when the data received
-                setData(data);
-            };
-
-            dataFetch();
-    }, [searchTerm]);
-
-    console.log(data)
+    let data = EventFetcher(`http://localhost:3000/search/${searchTerm}`)
 
     return (
-        <div>Search Results</div>
+        <>  
+            <EventType value={data}/>  
+        </>
     )
+}
+
+function EventType({ value, children, ...props }){
+    let eventMap = new Map();
+
+    for(var item = 0; item < value.length; item++) {
+        eventMap.set(
+            <Event 
+            id={value[item].id} 
+            title={value[item].title} 
+            startDate = {value[item].startDate}
+            endDate = {value[item].endDate}
+            shortDescription = {value[item].shortDescription}
+            longDescription = {value[item].longDescription}
+            image={value[item].image}
+            saved={false}
+            ></Event>
+        )     
+    }
+
+    if(value.length !== 0){
+        return (
+            <div style={{margin: "10px"}}>
+                <h1>Search Results</h1>
+                {eventMap}
+            </div>
+        ) 
+    }else {
+        return (
+            <div style={{margin: "10px"}}>
+                <h1>No Search Results</h1>
+            </div>
+        )
+    } 
 }
