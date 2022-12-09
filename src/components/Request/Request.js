8573fc1efd11d8dbb6b84f40698c8ae361b12129
berkanozturk <berkanozturk@ubuntu.myguest.virtualbox.org>
2022-12-08 16:37:26 -0800

    request page 12-08

--- src/components/Request/Request.js
+++ src/components/Request/Request.js
@@ -1,3 +1,16 @@
 export default function Request() {
-    return <h1>Request</h1>
+    return (
+    <div className="container-for-request-form">
+        <form className="request-form">
+            <h2 className="margin-bottom">Create Your Own Event</h2>
+            <input className="form-styling" type={"text"} id={"type"} placeholder={"Event Type"} required></input>
+            <input className="form-styling" type={"text"} id={"name"} placeholder={"Event Name"} required></input>
+            <input className="form-styling" type={"text"} id={"start"} placeholder={"Event Start Date"} required></input>
+            <input className="form-styling" type={"text"} id={"end"} placeholder={"Event End Date"} required></input>
+            <input className="form-styling" type={"text"} id={"shortdes"} placeholder={"Event Short Description"} required></input>
+            <input className="form-styling" type={"text"} id={"longdes"} placeholder={"Event Long Description"} required></input>
+            <button className="form-submit" type="submit">Submit</button>
+        </form>
+    </div>
+    );
 }
